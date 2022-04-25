5b828e71d0a0109574416a2c82cb385efda936e2
dvenomb98 <91594441+dvenomb98@users.noreply.github.com>
2022-04-25 14:12:53 +0200

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="bg-black flex justify-center items-center h-16 lg:h-32">
+    <p className="text-white font-semibold text-center p-5">© 2022 Clever lance</p>
+    </div>
+  )
+}
+
+export default Footer
